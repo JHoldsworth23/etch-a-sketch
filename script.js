272b139fc5e945a33b5cf5c4a9f6853367868447
JHoldsworth23 <jacobph101@icloud.com>
2023-11-03 20:29:41 +0000

    Change single quote mark to double quote mark

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Create a 16 x 16 grid
-const grid = document.querySelector('#grid');
+const grid = document.querySelector("#grid");
 const gridStyle = window.getComputedStyle(grid);
 const gridWidth = gridStyle.getPropertyValue("width").split("p")[0];
 const mouseToggle = false;
 
 for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+    const row = document.createElement("div");
+    row.classList.add("row");
     grid.appendChild(row);
 
     for (let j = 0; j < 16; j++) {
-        const block = document.createElement('div');
-        block.classList.add('block');
+        const block = document.createElement("div");
+        block.classList.add("block");
         block.style.width = gridWidth / 16 + "px";
         block.style.height = gridWidth / 16 + "px";
         row.appendChild(block);
         
-        block.addEventListener('mouseover', (event) => {
+        block.addEventListener("mouseover", (event) => {
             event.preventDefault();
-            block.style.backgroundColor = 'rgb(0, 0, 0)';
-            console.log('change the background color');
+            block.style.backgroundColor = "rgb(0, 0, 0)";
+            console.log("change the background color");
         });
     }
 }
