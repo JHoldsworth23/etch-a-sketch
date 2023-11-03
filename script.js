d9a0319a7517bb26ab0cad516de3277ab65d6855
JHoldsworth23 <jacobph101@icloud.com>
2023-11-03 13:12:56 +0000

    Create for loop to make 16 divs for the grid

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // Create a 16 x 16 grid
 const grid = document.querySelector('#grid');
+const block = document.createElement('div');
+
+block.classList.add('block');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    grid.appendChild(row);
+}
