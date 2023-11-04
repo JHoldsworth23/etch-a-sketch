844ff4a31aeb019b1dce90d74d03d8a7739d3438
JHoldsworth23 <jacobph101@icloud.com>
2023-11-04 15:34:01 +0000

    Refactor the code and use forEach method

--- script.js
+++ script.js
@@ -17,34 +17,25 @@ function createGrid(size) {
         grid.appendChild(square);
     }
 
-    // for (let i = 0; i < size; i++) {
-    //     const gridRow = document.createElement("div");
-    //     gridRow.classList.add("grid-row");
-    //     grid.appendChild(gridRow);
+    const allSquares = document.querySelectorAll(".grid-element");
 
-    //     for (let j = 0; j < size; j++) {
-    //         const gridElement = document.createElement("div");
-    //         gridElement.classList.add("grid-element");
-    //         gridElement.style.width = gridWidth / size + "px";
-    //         gridElement.style.height = gridWidth / size + "px";
-            
-    //         gridElement.addEventListener("mouseover", (event) => {
-    //             if (event.type === 'mouseover' && !mouseDown) {
-    //                 return;
-    //             } else {
-    //                 gridElement.style.backgroundColor = "#000000";
-    //             }
-    //         });
-    //         gridElement.addEventListener("mousedown", (event) => {
-    //             if (event.type === 'mouseover' && !mouseDown) {
-    //                 return;
-    //             } else {
-    //                 gridElement.style.backgroundColor = "#000000";
-    //             }
-    //         });
-    //         gridRow.appendChild(gridElement);
-    //     }
-    // }
+    allSquares.forEach((square) => {
+        square.addEventListener("mouseover", (event) => {
+            if (event.type === "mouseover" && !mouseDown) {
+                return;
+            } else {
+                square.style.backgroundColor = "rgb(0, 0, 0)";
+            }
+        });
+
+        square.addEventListener("mousedown", (event) => {
+            if (event.type === "mouseover" && !mouseDown) {
+                return;
+            } else {
+                square.style.backgroundColor = "rgb(0, 0, 0)";
+            }
+        });
+    });
 }
 
 createGrid(DEFAULTSIZE);
