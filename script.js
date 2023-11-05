ff4afc4d76085df83b49a33bf8ae0d2d0796edab
JHoldsworth23 <jacobph101@icloud.com>
2023-11-05 16:29:09 +0000

    Add the rainbow mode to dynamically change the colour

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const DEFAULT_SIZE = 24;
-const DEFAULT_COLOR = "rgb(0, 0, 0)";
+const DEFAULT_COLOUR = "rgb(0, 0, 0)";
+const DEFAULT_MODE = "colour";
 
 const grid = document.querySelector("#grid-container");
 const gridSize = document.querySelector("#grid-size");
+const gridText = document.querySelector(".grid-size-text");
 
 const colourWheel = document.querySelector("#colour-wheel");
-let colour = DEFAULT_COLOR;
+let colour = DEFAULT_COLOUR;
+
+const colourMode = document.querySelector(".colour-mode");
+const rainbowMode = document.querySelector(".rainbow-mode");
+let mode = DEFAULT_MODE;
 
 let mouseDown = false;
 document.body.onmousedown = () => {mouseDown = true}
@@ -27,21 +33,34 @@ function createGrid(size) {
 function drawGrid(event) {
     if (event.type === "mouseover" && !mouseDown) {
         return;
-    } else {
+    } else if (mode === "colour") {
         event.target.style.backgroundColor = colour;
+    } else if (mode === "rainbow") {
+        const red = Math.floor(Math.random() * 255);
+        const green = Math.floor(Math.random() * 255);
+        const blue = Math.floor(Math.random() * 255);
+
+        event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     }
 }
 
 createGrid(DEFAULT_SIZE);
 
-colourWheel.addEventListener('change', () => {
+colourWheel.addEventListener("change", () => {
     colour = colourWheel.value;
 });
 
 gridSize.addEventListener("change", () => {
-    let size = gridSize.value;
-
+    gridText.textContent = `Grid size: ${gridSize.value} x ${gridSize.value}`;
     grid.innerHTML = "";
 
-    createGrid(size);
+    createGrid(gridSize.value);
+});
+
+colourMode.addEventListener("click", () => {
+    mode = "colour"
+});
+
+rainbowMode.addEventListener("click", () => {
+    mode = "rainbow";
 });
