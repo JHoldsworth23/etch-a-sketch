ee2959b4ed67a75c737989132f9da69ceebea0e9
JHoldsworth23 <jacobph101@icloud.com>
2023-11-06 19:06:18 +0000

    Add function to darken or lighten RGB colour

--- script.js
+++ script.js
@@ -79,6 +79,31 @@ function createGrid(size) {
     }
 }
 
+function shadeColour(rgbColor, percent) {
+    rgbColorValue = rgbColor.substring(4, rgbColor.length - 1).split(",");
+    let red = parseInt(rgbColorValue[0]);
+    let green = parseInt(rgbColorValue[1]);
+    let blue = parseInt(rgbColorValue[2]);
+
+    red = parseInt(red * (100 + percent) / 100);
+    green = parseInt(green * (100 + percent) / 100);
+    blue = parseInt(blue * (100 + percent) / 100);
+
+    red = (red < 255) ? red : 255;
+    green = (green < 255) ? green : 255;
+    blue = (blue < 255) ? blue : 255;
+
+    red = Math.round(red);
+    green = Math.round(green);
+    blue = Math.round(blue);
+
+    const RR = ((red.toString(16).length == 1) ? "0"+red.toString(16) : red.toString(16));
+    const GG = ((green.toString(16).length == 1) ? "0"+green.toString(16) : green.toString(16));
+    const BB = ((blue.toString(16).length == 1) ? "0"+blue.toString(16) : blue.toString(16));
+
+    return `#${RR}${GG}${BB}`;
+}
+
 function draw(event) {
     if (event.type === "mouseover" && !mouseDown) {
         return;
@@ -89,6 +114,12 @@ function draw(event) {
         const green = Math.floor(Math.random() * 255);
         const blue = Math.floor(Math.random() * 255);
         event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    } else if (mode === "darken") {
+        const currentRGBColor = event.target.style.backgroundColor;
+        event.target.style.backgroundColor = shadeColour(currentRGBColor, -10);
+    } else if (mode === "lighten") {
+        const currentRGBColor = event.target.style.backgroundColor;
+        event.target.style.backgroundColor = shadeColour(currentRGBColor, 10);
     } else if (mode === "erase") {
         event.target.style.backgroundColor = "#FFFFFF";
     }
